df6522749c84ed91d5c70ca87bb304e091e5c3cd
Tai Lee <real.human@mrmachine.net>
2014-04-06 19:51:36 +1000

    Install `fluent-*` apps per instructions (master branches), and dependencies.

--- fluent_demo/settings.py
+++ fluent_demo/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
+    # 'django.contrib.admin', # Must come after `admin_tools`.
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -80,3 +80,190 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+### django ####################################################################
+
+# Explicitly define Django defaults, so we can override and extend.
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.contrib.messages.context_processors.messages',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+)
+
+### fluent-* ##################################################################
+
+# Common settings required by multiple `fluent-*` apps.
+
+INSTALLED_APPS += (
+    # 'django.contrib.comments',
+    'django.contrib.sites',
+    'django_wysiwyg',
+)
+
+DJANGO_WYSIWYG_FLAVOR = 'yui_advanced'
+SITE_ID = 1
+
+### fluent-blogs ##############################################################
+
+INSTALLED_APPS += (
+    'fluent_blogs',
+
+    # The content plugins
+    # 'fluent_contents',
+    # 'fluent_contents.plugins.text',
+
+    # Support libs
+    'categories',
+    'categories.editor',
+    # 'django_wysiwyg',
+
+    # Optional commenting support
+    # 'django.contrib.comments',
+
+    # Optional tagging
+    'taggit',
+    'taggit_autocomplete_modified',
+
+    # Optional fluent-pages support.
+    'fluent_blogs.pagetypes.blogpage',
+)
+
+### fluent-contents ###########################################################
+
+INSTALLED_APPS += (
+    'fluent_contents',
+
+    # And optionally all plugins desired:
+    'fluent_contents.plugins.code',
+    # 'fluent_contents.plugins.commentsarea',
+    # 'fluent_contents.plugins.disquswidgets',
+    'fluent_contents.plugins.formdesignerlink',
+    'fluent_contents.plugins.gist',
+    'fluent_contents.plugins.googledocsviewer',
+    'fluent_contents.plugins.iframe',
+    'fluent_contents.plugins.markup',
+    'fluent_contents.plugins.rawhtml',
+    'fluent_contents.plugins.text',
+
+    # Some plugins need extra Django applications
+    # 'disqus',
+    # 'django.contrib.comments',
+    # 'django_wysiwyg',
+    'form_designer',
+)
+
+### fluent-dashboard ##########################################################
+
+from django.utils.translation import ugettext_lazy as _
+
+INSTALLED_APPS += (
+    'fluent_dashboard',
+
+    # enable the admin
+    'admin_tools',
+    'admin_tools.theming',
+    'admin_tools.menu',
+    'admin_tools.dashboard',
+    'django.contrib.admin', # Must come after `admin_tools`.
+)
+
+ADMIN_TOOLS_INDEX_DASHBOARD = 'fluent_dashboard.dashboard.FluentIndexDashboard'
+ADMIN_TOOLS_APP_INDEX_DASHBOARD = 'fluent_dashboard.dashboard.FluentAppIndexDashboard'
+ADMIN_TOOLS_MENU = 'fluent_dashboard.menu.FluentMenu'
+
+# Optional app icons.
+FLUENT_DASHBOARD_APP_ICONS = {
+    # 'auth/user': 'user.png'
+}
+
+FLUENT_DASHBOARD_APP_GROUPS = (
+    (_('CMS'), {
+        'models': (
+            'cms.*',
+            'pages.*',
+            'fiber.*',
+        ),
+        'module': 'CmsAppIconList',
+        'collapsible': False,
+    }),
+    (_('Interactivity'), {
+        'models': (
+            'django.contrib.comments.*',
+            'form_designer.*'
+            'threadedcomments.*',
+            'zinnia.*',
+        ),
+    }),
+    (_('Administration'), {
+        'models': (
+            'django.contrib.auth.*',
+            'django.contrib.sites.*',
+            'google_analytics.*',
+            'registration.*',
+        ),
+    }),
+    (_('Applications'), {
+        'models': ('*',),
+        'module': 'AppList',
+        'collapsible': True,
+    }),
+)
+
+### Optional cache status ###
+
+INSTALLED_APPS += (
+    'dashboardmods',
+)
+
+# Example Memcache configuration:
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    },
+}
+
+# Optional, example Varnish configuration:
+VARNISH_MANAGEMENT_ADDRS = ('127.0.0.1:6082', )
+
+# Enable required `request` context processor.
+TEMPLATE_CONTEXT_PROCESSORS += (
+    'django.core.context_processors.request',
+)
+
+### fluent-pages ##############################################################
+
+INSTALLED_APPS += (
+    'fluent_pages',
+
+    # Required dependencies
+    'mptt',
+    'parler',
+    'polymorphic',
+    'polymorphic_tree',
+
+    # Optional widget pages via django-fluent-contents
+    'fluent_pages.pagetypes.fluentpage',
+    # 'fluent_contents',
+    # 'fluent_contents.plugins.text',
+    # 'django_wysiwyg',
+
+    # Optional other CMS page types
+    'fluent_pages.pagetypes.redirectnode',
+)
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'fluent-pages-templates'),
+)
+
+# FLUENT_PAGES_TEMPLATE_DIR = os.path.join(BASE_DIR, 'fluent-pages-templates')
+
+### south #####################################################################
+
+INSTALLED_APPS += (
+    'south',
+)
--- fluent_demo/urls.py
+++ fluent_demo/urls.py
@@ -10,3 +10,41 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+### fluent-blogs ##############################################################
+
+from fluent_blogs.sitemaps import \
+	EntrySitemap, CategoryArchiveSitemap, AuthorArchiveSitemap, \
+	TagArchiveSitemap
+
+sitemaps = {
+    'blog_entries': EntrySitemap,
+    'blog_categories': CategoryArchiveSitemap,
+    'blog_authors': AuthorArchiveSitemap,
+    'blog_tags': TagArchiveSitemap,
+}
+
+urlpatterns += patterns('',
+    url(r'^admin/util/taggit_autocomplete_modified/', include('taggit_autocomplete_modified.urls')),
+    # url(r'^blog/comments/', include('django.contrib.comments.urls')),
+    # url(r'^blog/', include('fluent_blogs.urls')), # Not needed if using `fluent_blogs.pagetypes.blogpage`.
+    url(r'^sitemap.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
+)
+
+### fluent-dashboard ##########################################################
+
+urlpatterns += patterns('',
+    url(r'^admintools/', include('admin_tools.urls')),
+)
+
+### form-designer #############################################################
+
+urlpatterns += patterns('',
+    (r'^forms/', include('form_designer.urls')),
+)
+
+### fluent-pages ##############################################################
+
+urlpatterns += patterns('',
+    url(r'^', include('fluent_pages.urls')) # Must come last, to catch all.
+)
